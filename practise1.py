49372590386f06d23ea5719679503c3fd5a035cd
Shivani Soni <sonishivani7987@gmail.com>
2026-01-09 13:46:45 +0530

    Day 3:  Python program practice

---
+++ practise1.py
@@ -0,0 +1,4 @@
+#  program to take age as input and print value entered and its  data  type
+age = input("enter your age :")  # by default input function takes input as string
+print("value entered is :", age)
+print("data type of age is :", type(age))2
